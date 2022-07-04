9c2ad1168288dc8117777f91d40fd3891b7f99fd
Malafur <36678107+Malafur@users.noreply.github.com>
2022-07-04 11:35:42 +0100

    New update
    Updated version to include a timer, remove duplicates, remove proxies above a certain score (by default it's 15) and displays a tally at the end with the results of how many duplicates, proxies above 15 score and 100 score there were.

--- Proxy scamalytics checker.py
+++ Proxy scamalytics checker.py
@@ -2,6 +2,7 @@ import time
 from time import sleep
 import requests
 from itertools import cycle
+from timeit import default_timer
 from lxml import html
 import pprint
 import sys
@@ -13,13 +14,19 @@ proxy = list(map(str.strip, userInput))
 
 pp = pprint.PrettyPrinter(indent=2)
 
+tally = {">15":0,
+         "100":0,
+         "Duplicates":0}
+
 proxlist = []
 iplist = {}
 
 proxy_pool = cycle(proxy)
-
+print("Starting\n")
+start = default_timer()
 
 for i in range(proxy_amount):
+    con = bool(True)
     session = requests.Session()
     next_proxy = next(proxy_pool)
     
@@ -29,9 +36,19 @@ for i in range(proxy_amount):
     #Goes onto next proxy if the current one times out
     except requests.exceptions.Timeout or ReadTimeout or RemoteDisconnected:
         continue
+
+    #Checks for duplicates and skips them if found
+    for m in proxlist:
+        if ip == m:
+            con = bool(False)
+
+    if con == bool(False):
+        tally["Duplicates"] = tally.get("Duplicates") + 1
+        continue
     
     proxlist.append(ip)
     iplist[i]=next_proxy, ip
+
 
 n = -1
 for x in proxlist:
@@ -39,9 +56,25 @@ for x in proxlist:
     url = "https://www.scamalytics.com/ip/" + x
     page = requests.get(url)
     #Pauses the code to let the page load, adjust for slower internet connections
-    time.sleep(0.2)
+    time.sleep(0.3)
     tree = html.fromstring(page.content)
-    score = tree.xpath('//div[@class="score"]/text()')
+    score = tree.xpath('//div[@class="score" and text()]/text()')
+
+    try:
+        mod_score = int(str(score).strip("[']").split()[2])
+    except ValueError:
+        pass
+
+    #Removes any results that are higher than 15
+    if mod_score > 15:
+        tally[">15"] = tally.get(">15") + 1
+        if mod_score == 100:
+            tally["100"] = tally.get("100") + 1
+        try:
+            iplist.pop(n)
+        except KeyError:
+            pass
+        continue
 
     #Adds the fraud score to the proxy
     try:
@@ -50,3 +83,7 @@ for x in proxlist:
         pass
 
 pp.pprint(iplist)
+print("\n")
+for x in tally:
+    print(x,": ",tally[x])
+print("\nTime: ", str(round((default_timer() - start), 3)))
